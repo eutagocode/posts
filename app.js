83fc8b7564d5799c9c24dd4e438624f5ee22c858
eutagocode <thiagoluix21@gmail.com>
2024-06-12 02:12:38 -0300

    refactor: :zap: Foi feita a melhoria na organização, separando as rotas em um arquivo único

--- app.js
+++ app.js
@@ -1,60 +1,11 @@
 const express = require("express");
-const uuid = require("uuid").v4;
+const routes = require("./routes/router");
 const app = express();
 const port = 9000;
 
-const posts = [
-    {
-        id: uuid(),
-        title: "Github",
-        desc: "O Github é o melhor",
-    },
-];
-
 app.use(express.json());
 
-app.get("/posts", (request, response) => {
-    return response.json(JSON.stringify(posts));
-});
-
-app.post("/posts/new", (request, response) => {
-    const { title, desc } = request.body;
-
-    const post = { id: uuid(), title, desc };
-
-    posts.push(post);
-
-    return response.json(post);
-});
-
-app.put("/posts/:id", (request, response) => {
-    const { id } = request.params;
-    const { title, desc } = request.body;
-
-    const postIndex = posts.findIndex(({ id }) => id == id);
-
-    if (postIndex < 0) {
-        return response.status(404).json({ error: "Post not found" });
-    }
-
-    posts[postIndex] = { id, title, desc };
-
-    return response.json("Post editado com sucesso!");
-});
-
-app.delete("/posts/:id", (request, response) => {
-    const { id } = request.params;
-
-    const postIndex = posts.findIndex(({ id }) => id == id);
-
-    if (postIndex < 0) {
-        return response.status(404).json({ error: "Post not found" });
-    }
-
-    posts.splice(postIndex, 1);
-
-    return response.json("Post deletado com sucesso!");
-});
+app.use("/api", routes);
 
 app.listen(port, () => {
     console.log("listening server on port", port);
---
+++ routes/router.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const router = express.Router();
+const uuid = require("uuid").v4;
+
+const posts = [
+    {
+        id: uuid(),
+        title: "Github",
+        desc: "O Github é o melhor",
+    },
+];
+
+router.get("/posts", (request, response) => {
+    return response.json(JSON.stringify(posts));
+});
+
+router.post("/posts/new", (request, response) => {
+    const { title, desc } = request.body;
+
+    const post = { id: uuid(), title, desc };
+
+    posts.push(post);
+
+    return response.json(post);
+});
+
+router.put("/posts/:id", (request, response) => {
+    const { id } = request.params;
+    const { title, desc } = request.body;
+
+    const postIndex = posts.findIndex((post) => post.id === id);
+
+    if (postIndex < 0) {
+        return response.status(404).json({ error: "Post not found" });
+    }
+
+    posts[postIndex] = { id, title, desc };
+
+    return response.json(posts);
+});
+
+router.delete("/posts/:id", (request, response) => {
+    const { id } = request.params;
+
+    const postIndex = posts.findIndex((post) => post.id === id);
+
+    if (postIndex < 0) {
+        return response.status(404).json({ error: "Post not found" });
+    }
+
+    posts.splice(postIndex, 1);
+
+    return response.json("Post deletado com sucesso!");
+});
+
+module.exports = router;
